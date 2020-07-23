ba664bc4a55925d9c46584be03c55ca4d1a715c2
nilesh767 <preneon@767>
2020-07-23 21:48:44 +0530

    refactored both versions of Pig Game

--- DOM-Pig Game/v2/app.js
+++ DOM-Pig Game/v2/app.js
@@ -1,54 +1,42 @@
-/*
-GAME RULES:
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-*/
+
 var scores, roundScore, activePlayer, dice, gamePlaying;
 
 init();
 
-//Roll btn
+
 document.querySelector('.btn-roll').addEventListener('click', function(){  
     
     if(gamePlaying){
-        //1. Random Number
+
         var dice = Math.floor(Math.random() * 6) + 1;
         var dicePrevious = dice;
-        //2. Display Result
+
         var diceDOM = document.querySelector('.dice');
         
         diceDOM.style.display  = 'block';
         diceDOM.src = 'dice-' + dice + '.png';
     
-        //3. Update the round score IF random number isn't 1
         if(dice !== 1 && (dice !== 6 && dicePrevious !== 6)){
-           //Add Score
-           roundScore += dice;
-           document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
         
         }else {
-           //Next Player
+
           nextPlayer();
 
         }
     }
 });
 
-//hold btn
 document.querySelector('.btn-hold').addEventListener('click', function(){
     
     if(gamePlaying){
         
-        // Add CURRENT score to GLOBAL score
         scores[activePlayer] += roundScore;
 
-        // Update the UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-        // Check if player won the game
         if (scores[activePlayer] >= 100) {
             
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
@@ -64,10 +52,8 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     }    
 });
 
-//new btn
 document.querySelector('.btn-new').addEventListener('click', init);
 
-//initialize data
 function init(){
     scores = [0, 0];
     activePlayer = 0;
@@ -89,7 +75,6 @@ function init(){
     document.querySelector('.player-0-panel').classList.add('active');
 }
 
-//change players
 function nextPlayer(){
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
