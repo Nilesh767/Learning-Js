10bf66bdbc190de2079bd0b0b12f8d888921ec15
nilesh767 <preneon@767>
2020-07-29 17:28:25 +0530

    modular  design pattern

---
+++ 4.Budget-App-Project/app.js
@@ -0,0 +1,13 @@
+var budgetController = (function() {
+    //some code
+})();
+
+
+var uiController = (function() {
+    //some code
+}());
+
+
+var controller = (function(budgetCtrl, uiCtrl) {
+    //some code
+})(budgetController, uiController);
