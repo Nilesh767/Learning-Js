90ad24025f74d9e8791321444767fde1476bc5c7
Nilesh Choudhary <33248629+Nilesh767@users.noreply.github.com>
2020-11-13 11:46:20 +0530

    dynamic output in jsx/component

--- 3reactjs/basics/src/Person/Person.js
+++ 3reactjs/basics/src/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const person = () => {
-    return <p>I'm a person!</p>
+    return <p> I'm a person and i am {Math.floor(Math.random()*100)} yo! </p>
 };
 
 export default person;
