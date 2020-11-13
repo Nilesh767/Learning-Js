881fa51d71b55ceafc90dd1c6e4cc0fd589f6327
Nilesh Choudhary <33248629+Nilesh767@users.noreply.github.com>
2020-11-13 11:55:22 +0530

    using props and its children property in jsx

--- 3reactjs/basics/src/App.js
+++ 3reactjs/basics/src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Henlo... </h1>
         <p>Working!</p>
-        <Person />
+        <Person name='Nilesh' age='21' />
+        <Person name='other name' age='20'>My hobby: some hobby</Person>
+        <Person name='some name' age='12' />
       </div>
       
     );
--- 3reactjs/basics/src/Person/Person.js
+++ 3reactjs/basics/src/Person/Person.js
@@ -1,6 +1,12 @@
 import React from 'react';
-const person = () => {
-    return <p> I'm a person and i am {Math.floor(Math.random()*100)} yo! </p>
+const person = (props) => {
+    return (
+        <div>
+            <p> I'm {props.name} and i am {props.age}yo! </p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 export default person;
+
