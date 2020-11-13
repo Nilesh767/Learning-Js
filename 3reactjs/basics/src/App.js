244ded5e6ba2b942cdea5ece923591f83df2d1f0
Nilesh Choudhary <33248629+Nilesh767@users.noreply.github.com>
2020-11-13 12:32:40 +0530

    getting started with states

--- 3reactjs/basics/src/App.js
+++ 3reactjs/basics/src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Person from'./Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Nilesh', age: 21 }, 
+      { name: 'Other name', age: 20 },
+      { name: 'some name', age: 12 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Henlo... </h1>
         <p>Working!</p>
-        <Person name='Nilesh' age='21' />
-        <Person name='other name' age='20'>My hobby: some hobby</Person>
-        <Person name='some name' age='12' />
+        <button>Switch name</button>
+        <Person name= {this.state.persons[0].name} age= {this.state.persons[0].age} />
+        <Person name= {this.state.persons[1].name} age= {this.state.persons[1].age} >My hobby: some hobby</Person>
+        <Person name= {this.state.persons[2].name} age= {this.state.persons[2].age}  />
       </div>
       
     );
