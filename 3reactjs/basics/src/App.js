76e2cfba0cb2c6b0fc46fbbbed9f3fec7ae09e8d
Nilesh Choudhary <33248629+Nilesh767@users.noreply.github.com>
2020-11-13 11:36:42 +0530

    creating functional components

--- 3reactjs/basics/src/App.js
+++ 3reactjs/basics/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from'./Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Henlo... </h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, 
-                                React.createElement('h1', null, 'henlooo'));
+    return (
+      <div className="App">
+        <h1>Henlo... </h1>
+        <p>Working!</p>
+        <Person />
+      </div>
+      
+    );
   }
 }
 
