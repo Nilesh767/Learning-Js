cb08edc373e7f3f4113fcf851c432c059eeba364
Nilesh Choudhary <33248629+Nilesh767@users.noreply.github.com>
2020-11-13 12:37:37 +0530

    handling events with methods(STATE)

--- 3reactjs/basics/src/App.js
+++ 3reactjs/basics/src/App.js
@@ -12,12 +12,16 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    console.log('Was clicked');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Henlo... </h1>
         <p>Working!</p>
-        <button>Switch name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name= {this.state.persons[0].name} age= {this.state.persons[0].age} />
         <Person name= {this.state.persons[1].name} age= {this.state.persons[1].age} >My hobby: some hobby</Person>
         <Person name= {this.state.persons[2].name} age= {this.state.persons[2].age}  />
