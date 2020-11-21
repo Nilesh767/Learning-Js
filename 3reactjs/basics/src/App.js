b51e28b55e056e0e41295b7bffc6bdf9ce95f07c
Nilesh Choudhary <33248629+Nilesh767@users.noreply.github.com>
2020-11-21 17:43:17 +0530

    using css modules for better styling and usage

--- 3reactjs/basics/src/App.js
+++ 3reactjs/basics/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import "./App.css";
+import classes from "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
   state = {
     persons: [
-      { id: '1', name: "Nilesh", age: 21 },
-      { id: 'a',name: "Other name", age: 20 },
-      { id: '2b', name: "some name", age: 12 },
+      { id: "1", name: "Nilesh", age: 21 },
+      { id: "a", name: "Other name", age: 20 },
+      { id: "2b", name: "some name", age: 12 },
     ],
     otherstate: "some other state",
     showPersons: false,
@@ -16,16 +16,16 @@ class App extends Component {
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons: persons})
-  }
+    this.setState({ persons: persons });
+  };
 
   nameChangeHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(per => {
+    const personIndex = this.state.persons.findIndex((per) => {
       return per.id === id;
     });
 
     const person = {
-      ...this.state.persons[personIndex]
+      ...this.state.persons[personIndex],
     };
 
     person.name = event.target.value;
@@ -33,7 +33,7 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({persons: persons});
+    this.setState({ persons: persons });
   };
 
   togglePersonsHandler = () => {
@@ -43,38 +43,45 @@ class App extends Component {
 
   render() {
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-                     click={() => this.deletePersonHandler(index)}
-                     name={person.name} 
-                     age={person.age}
-                     key={person.id}
-                     changed={(event) => this.nameChangeHandler(event, person.id)}
-                    />
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangeHandler(event, person.id)}
+              />
+            );
           })}
         </div>
       );
+
+      btnClass = classes.Red;
     }
 
-    const classes = [];
-    if (this.state.persons.length <=2) {
-      classes.push('red');// red
+    const assignedclasses = [];
+    if (this.state.persons.length <= 2) {
+      assignedclasses.push(classes.red); // red
     }
-    if (this.state.persons.length <=1) {
-      classes.push('bold');// red + bold
+    if (this.state.persons.length <= 1) {
+      assignedclasses.push(classes.bold); // red + bold
     }
 
     return (
-        <div className="App">
-          <h1>Henlo... hooman!!!</h1>
-          <p className={classes.join(' ')}>Working! :)</p>
-          <button className='button' onClick={this.togglePersonsHandler}>Toggle Persons</button>
-          {persons}
-        </div>
+      <div className={classes.App}>
+        <h1>Henlo... hooman!!!</h1>
+        <p className={assignedclasses.join(" ")}>Working! :)</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
+        {persons}
+      </div>
     );
   }
 }
