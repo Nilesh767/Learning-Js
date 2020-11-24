445e34156459f4414c1bfd824720f4937533edc7
Nilesh Choudhary <33248629+Nilesh767@users.noreply.github.com>
2020-11-24 18:47:34 +0530

    other form of HOCs

--- 3reactjs/2components_adv/src/hoc/WithClass.js
+++ 3reactjs/2components_adv/src/hoc/WithClass.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const withClass = props => (
-    <div className={props.classes}>
-        {props.children} 
+const withClass = (WrappedComponent, className) => {
+  return (props) => (
+    <div className={className}>
+      <WrappedComponent />
     </div>
-);
+  );
+};
 
-//high order component(hoc) may or may not serve a purpose(logic) 
-//div --> creates a div which take a property classes
-// props.Children --> part of data inside this resides in there
 export default withClass;
