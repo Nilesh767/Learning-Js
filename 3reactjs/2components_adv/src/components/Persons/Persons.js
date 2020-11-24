23bf8c8a917f28464f300a5da16879378dca544f
Nilesh Choudhary <33248629+Nilesh767@users.noreply.github.com>
2020-11-24 18:09:13 +0530

    purecomponents

--- 3reactjs/2components_adv/src/components/Persons/Persons.js
+++ 3reactjs/2components_adv/src/components/Persons/Persons.js
@@ -1,20 +1,24 @@
-import React, { Component } from "react";
+import React, {  PureComponent } from "react";
 import Person from "./Person/Person";
 
-class Persons extends Component {
+class Persons extends PureComponent {
   // static getDerivedStateFromProps(state, props){
   //   console.log('a.[Persons.js] getDerivedStateFromProps', props);
   //   return state;
   // } just commented out bcoz throwing error but working
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("b.[Persons.js] shouldComponentUpdate");
-    if (nextProps.persons !== this.props.persons) {
-      return true;
-    } else {
-      return false;
-    }
-  }// now rendering of persons won't happen again after remove cockpit
+  // shouldComponentUpdate(nextProps, nextState) {       --> purecomponent implements this automatically
+  //   console.log("b.[Persons.js] shouldComponentUpdate");
+  //   if (
+  //     nextProps.persons !== this.props.persons ||
+  //     nextProps.clicked !== this.props.clicked ||
+  //     nextProps.changed !== this.props.changed
+  //   ) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // } 
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
     console.log("d.[Persons.js] getSnapshotBeforeUpdate");
