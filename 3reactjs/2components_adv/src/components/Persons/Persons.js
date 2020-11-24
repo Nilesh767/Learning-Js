25f71601c983afbd5fa27831c84dc69285812a98
Nilesh Choudhary <33248629+Nilesh767@users.noreply.github.com>
2020-11-24 15:12:02 +0530

    using shouldComponentUpdate for optimization

--- 3reactjs/2components_adv/src/components/Persons/Persons.js
+++ 3reactjs/2components_adv/src/components/Persons/Persons.js
@@ -2,31 +2,34 @@ import React, { Component } from "react";
 import Person from "./Person/Person";
 
 class Persons extends Component {
-  
   // static getDerivedStateFromProps(state, props){
   //   console.log('a.[Persons.js] getDerivedStateFromProps', props);
   //   return state;
   // } just commented out bcoz throwing error but working
 
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('b.[Persons.js] shouldComponentUpdate');
-    return true;
-  }
+    console.log("b.[Persons.js] shouldComponentUpdate");
+    if (nextProps.persons !== this.props.persons) {
+      return true;
+    } else {
+      return false;
+    }
+  }// now rendering of persons won't happen again after remove cockpit
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log('d.[Persons.js] getSnapshotBeforeUpdate');
-    return {message: 'Snapshot!'};
+    console.log("d.[Persons.js] getSnapshotBeforeUpdate");
+    return { message: "Snapshot!" };
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log('e.[Persons.js] componentDidUpdate');
+    console.log("e.[Persons.js] componentDidUpdate");
     console.log(snapshot);
   }
 
   componentWillUnmount() {
-    console.log('[Persons.js] componentWillUnmount');
+    console.log("[Persons.js] componentWillUnmount");
   }
-  
+
   render() {
     console.log("c.4.[Persons.js] rendering");
     return this.props.persons.map((person, index) => {
