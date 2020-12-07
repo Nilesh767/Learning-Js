a239470422860e1879313593853af97f2c93eabf
Nilesh Choudhary <33248629+Nilesh767@users.noreply.github.com>
2020-12-07 18:23:23 +0530

    rendering components

--- 3reactjs/4httpreq_ajax/src/containers/Blog/Blog.js
+++ 3reactjs/4httpreq_ajax/src/containers/Blog/Blog.js
@@ -20,9 +20,9 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-      <Route path="/" exact render={() => <h1>Home</h1>} />
-      <Route path="/" render={() => <h1>New Post</h1>} />
-      <Posts />
+      {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+      <Route path="/" render={() => <h1>New Post</h1>} /> */}
+      <Route path="/" exact component={Posts} />
       </div>
     );
   }
