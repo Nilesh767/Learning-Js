082d8457525253735e96b6d8a46607895ee8d7a2
Nilesh Choudhary <33248629+Nilesh767@users.noreply.github.com>
2020-12-07 18:38:35 +0530

    swirching b/w pages

--- 3reactjs/4httpreq_ajax/src/containers/Blog/Blog.js
+++ 3reactjs/4httpreq_ajax/src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import {Route} from 'react-router-dom';
+import { Route, Link } from "react-router-dom";
 
 import "./Blog.css";
-import Posts from './Posts/Posts';
+import Posts from "./Posts/Posts";
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
   render() {
@@ -12,17 +13,20 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <a href="/new-post">New Post</a>
+                <Link to={{
+                  pathname: 'new-post',
+                  hash: '#submit',
+                  search: '?quick-submit=true'
+                }}>New Post</Link>
               </li>
             </ul>
           </nav>
         </header>
-      {/* <Route path="/" exact render={() => <h1>Home</h1>} />
-      <Route path="/" render={() => <h1>New Post</h1>} /> */}
-      <Route path="/" exact component={Posts} />
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" exact component={NewPost} />
       </div>
     );
   }
