ec9f438815b5acb89fa008becb5132f124f13d99
Nilesh Choudhary <33248629+Nilesh767@users.noreply.github.com>
2020-12-07 16:37:19 +0530

    Setting up for react-routing

--- 3reactjs/4httpreq_ajax/src/containers/Blog/Blog.js
+++ 3reactjs/4httpreq_ajax/src/containers/Blog/Blog.js
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
-//import axios from "axios";
-import axios from '../../axios';
+import axios from "../../axios";
 
 import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 
 class Blog extends Component {
@@ -52,14 +49,20 @@ class Blog extends Component {
       });
     }
     return (
-      <div>
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">New Post</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
       </div>
     );
   }
