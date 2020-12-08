8504320dc05773acc74d85857e6e8057eadf8b77
Nilesh Choudhary <33248629+Nilesh767@users.noreply.github.com>
2020-12-08 20:35:46 +0530

    Navigating PRogrammatically

--- 3reactjs/4httpreq_ajax/src/containers/Blog/Blog.js
+++ 3reactjs/4httpreq_ajax/src/containers/Blog/Blog.js
@@ -41,8 +41,8 @@ class Blog extends Component {
           </nav>
         </header>
 
+        <Route path="/" exact component={Posts} />{" "}
         <Switch>
-          <Route path="/" exact component={Posts} />{" "}
           <Route path="/new-post" exact component={NewPost} />
           <Route path="/:id" exact component={FullPost} />
         </Switch>
--- 3reactjs/4httpreq_ajax/src/containers/Blog/Posts/Posts.js
+++ 3reactjs/4httpreq_ajax/src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "../../../axios";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 import "./Posts.css";
 import Post from "../../../components/Post/Post";
@@ -31,7 +31,8 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
+    //this.setState({ selectedPostId: id });
+    this.props.history.push({pathname: "/" + id});
   };
 
   render() {
@@ -39,13 +40,14 @@ class Posts extends Component {
     if (!this.state.error) {
       posts = this.state.posts.map((post) => {
         return (
-          <Link to={"/" + post.id} key={post.id}>
-            <Post
-              title={post.title}
-              author={post.author}
-              clicked={() => this.postSelectedHandler(post.id)}
-            />
-          </Link>
+          //<Link to={"/" + post.id} key={post.id}>
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+          //</Link>
         );
       });
       return <section className="Posts">{posts}</section>;
