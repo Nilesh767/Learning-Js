a6f0689badc593a6835ee78fc63b5c38de6848b3
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2020-12-11 18:47:51 +0530

    using react.Suspense for async(lazy) loading

--- 3reactjs/4httpreq_ajax/src/containers/Blog/Blog.js
+++ 3reactjs/4httpreq_ajax/src/containers/Blog/Blog.js
@@ -1,20 +1,17 @@
-import React, { Component } from "react";
-import { Route, NavLink, Switch, Redirect } from "react-router-dom";
+import React, { Component, Suspense } from "react";
+import { Route, NavLink, Switch } from "react-router-dom";
 
 import "./Blog.css";
 import Posts from "./Posts/Posts";
 //import NewPost from "./NewPost/NewPost";
-import asyncComponent from '../../hoc/asyncComponent/asyncComponent';
+//import asyncComponent from '../../hoc/asyncComponent/asyncComponent';
 
-const AsyncComponent = asyncComponent(() => {
-  return import("./NewPost/NewPost");
-});
+const AsyncComponent = React.lazy(() => import("./NewPost/NewPost"));
 
 class Blog extends Component {
-
   state = {
-    auth: true
-  }
+    auth: true,
+  };
 
   render() {
     return (
@@ -51,7 +48,17 @@ class Blog extends Component {
         </header>
 
         <Switch>
-          {this.state.auth ? <Route path="/new-post" exact component={AsyncComponent} /> : null}
+          {this.state.auth ? (
+            <Route
+              path="/new-post"
+              exact
+              render={() => (
+                <Suspense fallback={<div>Loading...</div>}>
+                  <AsyncComponent />
+                </Suspense>
+              )}
+            />
+          ) : null}
           <Route path="/posts" component={Posts} />
           <Route render={() => <h1>Not Found</h1>} />
           {/*<Redirect to="/posts" from="/" />*/}
