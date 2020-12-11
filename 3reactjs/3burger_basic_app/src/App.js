0eee22558a6de6e3ebb271e8c5a9dde0651aa05c
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2020-12-11 21:16:49 +0530

    Adding checkout and orders

--- 3reactjs/3burger_basic_app/src/App.js
+++ 3reactjs/3burger_basic_app/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
-import Layout from './hoc/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/burgerBuilder';
+
+import Layout from "./hoc/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/burgerBuilder";
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   render() {
     return (
-    <div>
-      <Layout>
-		<BurgerBuilder />
-      </Layout>
-    </div>
+      <div>
+        <Layout>
+          <BurgerBuilder />
+          <Checkout />
+        </Layout>
+      </div>
     );
   }
 }
