90ac7f1e5d066e92f9d66656e854d5932baaca6b
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2020-12-12 16:39:23 +0530

    fetching and displaying orders

--- 3reactjs/3burger_basic_app/src/containers/Orders/Orders.js
+++ 3reactjs/3burger_basic_app/src/containers/Orders/Orders.js
@@ -1,16 +1,45 @@
 import React, { Component } from "react";
 
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/WithErrorHandler/withErrorHandler";
+
 import Order from "../../components/Order/Order";
 
 class Orders extends Component {
-    render () {
-        return (
-            <div>
-                <Order />
-                <Order />
-            </div>
-        );
-    }
+  state = {
+    orders: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then((response) => {
+        const fetchedOrders = [];
+        for (let key in response.data) {
+          fetchedOrders.push({
+            ...response.data[key],
+            id: key,
+          });
+        }
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch((errors) => {
+        this.setState({ loading: false });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.orders.map(order => (
+            <Order key={order.id}
+            ingredients={order.ingredients}
+            price={order.price} />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
