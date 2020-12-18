5c375ed74083ab57fbdbe18492c2cc22af5cbfcc
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2020-12-18 17:36:03 +0530

    Connecting Store to react using react-redux lib

--- 3reactjs/5redux/src/index.js
+++ 3reactjs/5redux/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
@@ -11,7 +12,9 @@ const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
