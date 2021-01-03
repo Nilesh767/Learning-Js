832d4d5740ab7cab4622269429c73e0be0d5eef8
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2021-01-03 12:49:48 +0530

    using actionCreators and getState

--- 3reactjs/5redux/src/store/actions/result.js
+++ 3reactjs/5redux/src/store/actions/result.js
@@ -1,15 +1,19 @@
 import * as actionTypes from "./actionTypes";
 
 export const saveResult = (result) => {
+  // const updatedResult = result * 2;
   return {
     type: actionTypes.STORE_RESULT,
     result: result,
+    //result : updatedResult,
   };
 };
 
 export const storeResult = (result) => {
-  return (dispatch) => {
+  return (dispatch, getState) => {
     setTimeout(() => {
+      const oldCounter = getState().ctr.counter;
+      console.log(oldCounter);
       dispatch(saveResult(result));
     }, 2000);
   };
