f51991b9868426f0f25f0af0f1186e5ef0c2388d
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2021-01-08 17:37:18 +0530

    Adding Auth Form

--- 3reactjs/3burger_basic_app/src/App.js
+++ 3reactjs/3burger_basic_app/src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Layout from "./hoc/Layout/Layout";
-import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders";
+import Auth from "./containers/Auth/Auth";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Switch>
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
