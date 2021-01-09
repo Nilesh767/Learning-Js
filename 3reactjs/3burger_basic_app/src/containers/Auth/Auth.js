9cf10b8dca4843b442d3a0047519d28f75bec40e
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2021-01-09 15:32:33 +0530

    Adding Auth actions

--- 3reactjs/3burger_basic_app/src/containers/Auth/Auth.js
+++ 3reactjs/3burger_basic_app/src/containers/Auth/Auth.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import classes from "./Auth.css";
+
+import * as actions from "../../store/actions/index";
 
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
@@ -77,6 +80,14 @@ class Auth extends Component {
     this.setState({ controls: updatedControls });
   };
 
+  submitHandler = (event) => {
+    event.preventDefault();
+    this.props.onAuth(
+      this.state.controls.email.value,
+      this.state.controls.password.value,
+    );
+  };
+
   render() {
     const formElementArray = [];
     for (let key in this.state.controls) {
@@ -98,9 +109,10 @@ class Auth extends Component {
         changed={(event) => this.inputChangedHandler(event, formElement.id)}
       />
     ));
+
     return (
       <div className={classes.Auth}>
-        <form>
+        <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
         </form>
@@ -109,4 +121,10 @@ class Auth extends Component {
   }
 }
 
-export default Auth;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAuth: (email, password) => dispatch(actions.auth(email, password)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
