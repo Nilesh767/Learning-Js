9cbba48e0ada79fd444c9a72812e173782211c63
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2021-01-21 20:58:39 +0530

    Working with nextjs

---
+++ 3reactjs/7nextjs/pages/auth/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AuthIndexPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Auth Page</h1>
+      </div>
+    );
+  }
+}
+
+export default AuthIndexPage;
