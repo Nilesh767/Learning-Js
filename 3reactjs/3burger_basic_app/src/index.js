15a30765cb2decce3689b227ecde237a614e5606
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2021-01-24 20:30:43 +0530

    Adding Logout saga

--- 3reactjs/3burger_basic_app/src/index.js
+++ 3reactjs/3burger_basic_app/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, compose, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
 import "./index.css";
 import App from "./App";
@@ -12,6 +13,8 @@ import reportWebVitals from "./reportWebVitals";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
+
+import { watchAuth } from "./store/sagas/";
 
 const composeEnhancers =
   process.env.NODE_ENV === "development"
@@ -24,10 +27,14 @@ const rootReducer = combineReducers({
   auth: authReducer,
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
 );
+
+sagaMiddleware.run(watchAuth);
 
 const app = (
   <Provider store={store}>
