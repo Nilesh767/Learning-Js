c5cb865e716282ac887998129aaa15cf445d119a
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2021-01-27 13:54:49 +0530

    Using useContext for login

--- 3reactjs/9react_hooks/src/index.js
+++ 3reactjs/9react_hooks/src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
 import App from "./App";
+import AuthContextProvider from "./context/auth-context";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
